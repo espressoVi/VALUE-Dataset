46213d5d6d4bad204962c96891b8b60052142088
Soumadeep Saha <soumadeep.saha97@gmail.com>
2023-08-24 03:24:50 +0530

    Finished dataset creation, prepare to freeze

--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-blender board.blend -b -P render.py -- 0 40000 > /dev/null &
-blender board.blend -b -P render.py -- 40000 80000 > /dev/null &
-blender board.blend -b -P render.py -- 80000 120000 > /dev/null &
-blender board.blend -b -P render.py -- 120000 160000 > /dev/null &
-blender board.blend -b -P render.py -- 160000 200000 > /dev/null &
-blender board.blend -b -P render.py -- 200000 220000 > /dev/null &
